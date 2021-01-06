624f6b7c0bcd79c5c3c5bbf33d99850dd04a7715
Anjali Gupta <64606152+anjalig21@users.noreply.github.com>
2021-01-05 21:58:29 -0500

    Added the contours function which detects the pen

--- index.py
+++ index.py
@@ -1,27 +1,89 @@
-import numpy as np
 import cv2
-from collections import deque
+import numpy as np
 
-# Defining upper and lower bounds for a colour to be considered Orange
-orangeLower = np.array([100, 60, 60])
-orangeUpper = np.array([140, 255, 255])
+frameWidth = 640
+frameHeight = 480
 
-# Defining a matrix to add pixels to the boundaries of objects in an image (dilation)
-# and to remove pixels on object boundaries (erosion).
-matrix = np.ones((5,5)), np.uint8)
+# Initializing Webcam
+cap = cv2.VideoCapture(0)
 
-# Initialize different deques to store colours in arrays
-blue = [deque(maxlen=512)]
-green = [deque(maxlen=512)]
-red = [deque(maxlen=512)]
-yellow = [deque(maxlen=512)]
+# Width = ID #3, Height = ID #4, Brightness = ID #10
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
 
-#Initialize index variable for all colours
-blue_index = 0
-green_index = 0
-red_index = 0
-yellow_index = 0
+# Hue_min/max, Sat_min/max, Value_min/max,
+myColours = [[96, 10, 75, 116, 182, 167],
+             [43, 0, 49, 90, 182, 120],
+             [147, 81, 93, 179, 147, 156],
+             [16, 100, 93, 31, 255, 160]]
 
-# Blue, Green, Red, Yellow respectively
-colours = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-colourIndex = 0
+
+# Values are in BGR not RGB
+myColourValues = [[204, 204, 0],
+                  [0, 153, 0],
+                  [204, 0, 204],
+                  [0, 255, 255]]
+
+
+# [x, y, colourID]
+myPoints = []
+
+
+# Used to recognize colour and track points for drawing
+def findColour(img, myColours, myColourValues):
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    counter = 0
+    newPoints = []
+    for colour in myColours:
+        lower = np.array(colour[0:3])
+        upper = np.array(colour[3:6])
+        mask = cv2.inRange(imgHSV, lower, upper)
+        x, y = get_contours(mask)
+        cv2.circle(imgResult, (np.float32(x), np.float32(y)), 15, myColourValues[counter], cv2.FILLED)
+        if x != 0 and y != 0:
+            newPoints.append([x, y, counter])
+        counter += 1
+    return newPoints
+
+
+# Used to find tip of pen
+def get_contours(img):
+    # cv2.RETR_EXTERNAL good to find outer corners/details
+    contours, hierarchy = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    x, y, w, h = 0, 0, 0, 0
+    for cnt in contours:
+        area_og = int(cv2.contourArea(cnt))
+        if area_og > 50:
+            area = int(cv2.contourArea(cnt))
+            perimeter = int(cv2.arcLength(cnt, True))
+            # Calculates vertices
+            approx_points = cv2.approxPolyDP(cnt, 0.02*(perimeter), True)
+            # gives x, y, width and height of all shapes
+            x, y, w, h = cv2.boundingRect(approx_points)
+    return x+w/2, y
+
+
+# Used to draw all points
+def draw(myPoints, myColourValues):
+    for point in myPoints:
+        cv2.circle(imgResult, (np.float32(point[0]), np.float32(point[1])),
+                   5, myColourValues[point[2]], cv2.FILLED)
+
+
+while True:
+    _, image = cap.read(0)
+    img = cv2.flip(image, 1)
+    imgResult = img.copy()
+    newPoints = findColour(img, myColours, myColourValues)
+    if len(newPoints) != 0:
+        for newpt in newPoints:
+            myPoints.append(newpt)
+    if len(myPoints) != 0:
+        draw(myPoints, myColourValues)
+    cv2.imshow("Result", imgResult)
+
+    # if 'q' is pressed, webcam turns off
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
