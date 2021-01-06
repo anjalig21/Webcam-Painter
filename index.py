ff41e60a60293ee939b94360f6e154bf80a06368
Anjali Gupta <anjaligta@outook.com>
2021-01-05 22:29:20 -0500

    added GIF

--- index.py
+++ index.py
@@ -1,27 +1,13 @@
-import numpy as np
 import cv2
-from collections import deque
+camera = cv2.VideoCapture(0)
 
-# Defining upper and lower bounds for a colour to be considered Orange
-orangeLower = np.array([100, 60, 60])
-orangeUpper = np.array([140, 255, 255])
+# Keep looping
+while True:
+    # Grab the current paintWindow
+    (grabbed, frame) = camera.read()
+    frame = cv2.flip(frame, 1)
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-# Defining a matrix to add pixels to the boundaries of objects in an image (dilation)
-# and to remove pixels on object boundaries (erosion).
-matrix = np.ones((5,5)), np.uint8)
-
-# Initialize different deques to store colours in arrays
-blue = [deque(maxlen=512)]
-green = [deque(maxlen=512)]
-red = [deque(maxlen=512)]
-yellow = [deque(maxlen=512)]
-
-#Initialize index variable for all colours
-blue_index = 0
-green_index = 0
-red_index = 0
-yellow_index = 0
-
-# Blue, Green, Red, Yellow respectively
-colours = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (0, 255, 255)]
-colourIndex = 0
+# Cleanup the camera and close any open windows
+camera.release()
+cv2.destroyAllWindows()
